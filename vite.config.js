a14c4d889b7933ad77b7e2d37e84698d09ea79ee
AHMED2013K <162152543+AHMED2013K@users.noreply.github.com>
2026-02-11 09:20:11 +0000

    Clarify base path comment in vite.config.js
    Updated base path comment for clarity regarding GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // <-- root for custom domain
+  base: '/', // since your GitHub Pages is using a custom domain, or '/' if normal repo
 });
