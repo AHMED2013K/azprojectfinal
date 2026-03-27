d9769ebb37a56cb3795b9b87b8dddfd1ee524552
Ahmed Kalboussi <ahmedk2013k@gmail.com>
2026-03-27 16:23:58 +0000

    Fix UI typos and code corrections

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,20 @@ import react from '@vitejs/plugin-react';
 import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
-<<<<<<< HEAD
   plugins: [
     react(),
     sitemap({
       hostname: 'https://edugrowth.tn',
       urls: [
         '/',
-        '/partners',
-        '/services',
-        '/contact',
-        '/fr'
-      ]
-    })
+        '/abroad-zone',
+        '/outsourcing',
+        '/book-consultation',
+        '/blog',
+      ],
+    }),
   ],
-  base: '/', // root of your custom domain
+  base: '/',
   build: {
     rollupOptions: {
       output: {
@@ -29,8 +28,4 @@ export default defineConfig({
     },
     chunkSizeWarningLimit: 1000,
   },
-=======
-  plugins: [react()],
-  base: '/', // since your GitHub Pages is using a custom domain, or '/' if normal repo
->>>>>>> 4ef3a7e812eec063389865b86568f1f5db7963b7
 });
