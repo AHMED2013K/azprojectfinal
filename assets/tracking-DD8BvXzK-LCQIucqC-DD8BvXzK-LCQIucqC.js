22f026e537e664ada37e9b8359d6b486b0baace1
Ahmed Kalboussi <ahmedk2013k@gmail.com>
2026-04-13 17:25:04 +0100

    Updates

--- assets/tracking-DD8BvXzK-LCQIucqC-DD8BvXzK-LCQIucqC.js
+++
@@ -1,1 +0,0 @@
-function t(a,n={}){try{window.dataLayer&&Array.isArray(window.dataLayer)&&window.dataLayer.push({event:a,...n}),typeof window.gtag=="function"&&window.gtag("event",a,n)}catch{}}export{t as n};
